f9e58daa1f78f2b9c639a9ce7e1129341dbda762
Seraphim <seraphim.codes@gmail.com>
2025-06-11 22:40:50 -0400

    Planned out in route.ts next steps for dynamic room creation

---
+++ src/app/api/create-room/route.ts
@@ -0,0 +1,5 @@
+// step 1: add daily api key to env
+// step 2: create api route (in this file)
+// step 3: call the server endpoint from the client when a match is confirmed(probably in the /page.tsx)
+// step 4: use the returned URL from the server inside the /[sessionId] page.tsx
+// step 5: set an expiry for each room.
