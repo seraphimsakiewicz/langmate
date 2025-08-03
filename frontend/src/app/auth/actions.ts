56e4b14d88c72ba919d6cc129e80734df41840fa
Seraphim <seraphim.codes@gmail.com>
2025-08-02 22:39:56 -0400

    Reset account-form trying to fix auth bug on sign in / sign-up and redirect to /account

--- frontend/src/app/auth/actions.ts
+++ frontend/src/app/auth/actions.ts
@@ -37,8 +37,6 @@ export async function signup(formData: FormData) {
 
   const { error } = await supabase.auth.signUp(data)
 
-  console.log("error", error)
-
   if (error) {
     redirect('/error')
   }
