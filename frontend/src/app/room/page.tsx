d546debd5508e1351fcd204f4612f03287903640
Seraphim <seraphim.codes@gmail.com>
2025-09-13 10:13:28 -0400

    Established connection w livekit, but no video still

--- frontend/src/app/room/page.tsx
+++ frontend/src/app/room/page.tsx
@@ -7,63 +7,112 @@ import {
   RoomAudioRenderer,
   useTracks,
   RoomContext,
+  PreJoin,
+  LocalUserChoices,
+  VideoConference,
 } from "@livekit/components-react";
-import { Room, Track } from "livekit-client";
+import {
+  Room,
+  Track,
+  VideoCaptureOptions,
+  TrackPublishDefaults,
+  VideoPresets,
+  VideoCodec,
+} from "livekit-client";
 import "@livekit/components-styles";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+type ConnectionDetails = {
+  serverUrl: string;
+  roomName: string;
+  participantName: string;
+  participantToken: string;
+};
 
 export default function Page() {
   // TODO: get user input for room and name
   const room = "quickstart-room";
   const name = "quickstart-user";
-  const [roomInstance] = useState(
-    () =>
-      new Room({
-        // Optimize video quality for each participant's screen
-        adaptiveStream: true,
-        // Enable automatic audio/video quality optimization
-        dynacast: true,
-      })
+  const [roomInstance, setRoomInstance] = useState<Room | undefined>(undefined);
+  const [preJoinChoices, setPreJoinChoices] = useState<LocalUserChoices | undefined>(undefined);
+  const [connectionDetails, setConnectionDetails] = useState<ConnectionDetails | undefined>(
+    undefined
   );
-  const [isLoaded, setLoaded] = useState<boolean>(false);
 
-  useEffect(() => {
-    let mounted = true;
-    (async () => {
-      try {
-        const resp = await fetch(`/api/livekit-token?room=${room}&username=${name}`);
-        const data = await resp.json();
-        if (!mounted) return;
-        if (data.token) {
-          setLoaded(true);
-          await roomInstance.connect(process.env.NEXT_PUBLIC_LIVEKIT_URL || "", data.token);
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    })();
-
-    return () => {
-      mounted = false;
-      roomInstance.disconnect();
+  const preJoinDefaults = useMemo(() => {
+    return {
+      username: "",
+      videoEnabled: true,
+      audioEnabled: true,
     };
-  }, [roomInstance]);
+  }, []);
 
-  if (!isLoaded) {
-    return <p>Token loading...</p>;
-  }
+  const handlePreJoinSubmit = useCallback(async (values: LocalUserChoices) => {
+    setPreJoinChoices(values);
+    const resp = await fetch(`/api/livekit-token?room=${room}&username=${name}`);
+    const data = await resp.json();
+    setConnectionDetails(data);
+
+    console.log("connectionDetails", data);
+
+    const videoCaptureDefaults: VideoCaptureOptions = {
+      deviceId: values.videoDeviceId ?? undefined,
+      resolution: VideoPresets.h720,
+    };
+
+    const publishDefaults: TrackPublishDefaults = {
+      dtx: false,
+      videoSimulcastLayers: [VideoPresets.h1080, VideoPresets.h720],
+      videoCodec: "vp9",
+    };
+
+    const newRoom = {
+      videoCaptureDefaults: videoCaptureDefaults,
+      //   publishDefaults: publishDefaults,
+      audioCaptureDefaults: {
+        deviceId: values.audioDeviceId ?? undefined,
+      },
+    };
+
+    const roomInstance = new Room(newRoom);
+
+    setRoomInstance(roomInstance);
+
+    roomInstance.connect(process.env.NEXT_PUBLIC_LIVEKIT_URL || "", data.token, {
+      autoSubscribe: true,
+    });
+  }, []);
+
+  const handlePreJoinError = useCallback((e: any) => console.error(e), []);
 
   return (
-    <RoomContext.Provider value={roomInstance}>
-      <div data-lk-theme="default" style={{ height: "100dvh" }}>
-        {/* Your custom component with basic video conferencing functionality. */}
-        <MyVideoConference />
-        {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
-        <RoomAudioRenderer />
-        {/* Controls for the user to start/stop audio, video, and screen share tracks */}
-        <ControlBar />
-      </div>
-    </RoomContext.Provider>
+    <main data-lk-theme="default" style={{ height: "100%" }}>
+      {connectionDetails === undefined || preJoinChoices === undefined ? (
+        <div style={{ display: "grid", placeItems: "center", height: "100%" }}>
+          <PreJoin
+            defaults={preJoinDefaults}
+            onSubmit={handlePreJoinSubmit}
+            onError={handlePreJoinError}
+          />
+        </div>
+      ) : (
+        <RoomContext.Provider value={roomInstance}>
+          <p>RoomInstance!!!! {JSON.stringify(roomInstance)}</p>
+          <div data-lk-theme="default" style={{ height: "100dvh" }}>
+            <VideoConference
+            //   chatMessageFormatter={formatChatMessageLinks}
+            //   SettingsComponent={SHOW_SETTINGS_MENU ? SettingsMenu : undefined}
+            />
+            {/* Your custom component with basic video conferencing functionality. */}
+            {/* <MyVideoConference /> */}
+            {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
+            {/* <RoomAudioRenderer /> */}
+            {/* Controls for the user to start/stop audio, video, and screen share tracks */}
+            <ControlBar />
+          </div>
+        </RoomContext.Provider>
+      )}
+    </main>
   );
 }
 
