13da76d5c2951ffc5e976b77018afb473f88ef1f
Seraphim <seraphim.codes@gmail.com>
2025-09-17 17:43:33 -0400

    Fixed issue with second-nav styles for navItems

--- frontend/src/components/layout/second-nav.tsx
+++ frontend/src/components/layout/second-nav.tsx
@@ -14,6 +14,7 @@ interface NavItemProps {
   isActive: boolean;
   children: React.ReactNode;
   className?: string;
+  href: string;
 }
 export const SecondNav = () => {
   const pathName = usePathname();
@@ -45,23 +46,17 @@ export const SecondNav = () => {
             <MenuIcon className="size-[1.5rem]" />
           </Button>
 
-          <NavItem isActive={currentView === "calendar"}>
-            <Link href="/calendar">
-              <CalendarIcon className="mr-2 h-4 w-4" />
-              Calendar
-            </Link>
+          <NavItem href="/calendar" isActive={currentView === "calendar"}>
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            Calendar
           </NavItem>
-          <NavItem isActive={currentView === "sessions"}>
-            <Link href="/sessions">
-              <UsersIcon className="mr-2 h-4 w-4" />
-              Sessions
-            </Link>
+          <NavItem href="/sessions" isActive={currentView === "sessions"}>
+            <UsersIcon className="mr-2 h-4 w-4" />
+            Sessions
           </NavItem>
-          <NavItem isActive={currentView === "people"}>
-            <Link href="/people">
-              <UserIcon className="mr-2 h-4 w-4" />
-              People
-            </Link>
+          <NavItem href="/people" isActive={currentView === "people"}>
+            <UserIcon className="mr-2 h-4 w-4" />
+            People
           </NavItem>
         </div>
       </div>
@@ -69,15 +64,15 @@ export const SecondNav = () => {
   );
 };
 
-const NavItem = ({ isActive, children, className = "" }: NavItemProps) => (
-  <Button
-    variant="ghost"
-    className={`px-4 py-2 ${className} ${
+const NavItem = ({ isActive, children, href, className = "" }: NavItemProps) => (
+  <Link
+    href={href}
+    className={`inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all px-4 py-2 ${className} ${
       isActive
-        ? "bg-calendar-primary/10 text-black pointer-events-none font-medium border-b-2 border-calendar-primary"
+        ? "bg-primary/10 text-black pointer-events-none font-medium border-b-2 border-primary"
         : "text-muted-foreground hover:text-foreground hover:underline"
     }`}
   >
     {children}
-  </Button>
+  </Link>
 );
