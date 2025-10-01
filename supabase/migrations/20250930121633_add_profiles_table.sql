0c95852768a5e51892b63acf7825ccc02f4f0e24
Seraphim <seraphim.codes@gmail.com>
2025-10-01 18:13:15 -0400

    Removed comments from profiles table

--- supabase/migrations/20250930121633_add_profiles_table.sql
+++ supabase/migrations/20250930121633_add_profiles_table.sql
@@ -3,9 +3,7 @@ CREATE TABLE public.profiles (
   "email" varchar(255) UNIQUE NOT NULL,
   "first_name" varchar(50) NOT NULL,
   "last_name" varchar(50) NOT NULL,
-  -- "avatar_url" text,
   "timezone" varchar(50) NOT NULL,
-  -- "bio" text,
   "created_at" timestamptz DEFAULT (now()),
   "updated_at" timestamptz DEFAULT (now())
 );
