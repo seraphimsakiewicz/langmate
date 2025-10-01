75b13a8871744346be47a48371374f2d39fb5d9c
Seraphim <seraphim.codes@gmail.com>
2025-10-01 19:19:53 -0400

    Fixed the order of table creation for first migration file

--- supabase/migrations/20250930121633_add_profiles_languages_table.sql
+++ supabase/migrations/20250930121633_add_profiles_languages_table.sql
@@ -1,16 +1,3 @@
-CREATE TABLE public.profiles (
-  "id" uuid PRIMARY KEY REFERENCES auth.users(id) ON DELETE CASCADE,
-  "email" varchar(255) UNIQUE NOT NULL,
-  "first_name" varchar(50) NOT NULL,
-  "last_name" varchar(50) NOT NULL,
-  "timezone" varchar(50) NOT NULL,
-  "created_at" timestamptz DEFAULT (now()),
-  "updated_at" timestamptz DEFAULT (now()),
-  "native_language_id" uuid NOT NULL REFERENCES public.languages("id"),
-  "target_language_id" uuid NOT NULL REFERENCES public.languages("id"),
-  "target_level" varchar(20) NOT NULL CHECK (target_level IN ('beginner', 'intermediate', 'advanced'))
-);
-
 CREATE TABLE public.languages (
   "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
   "code" varchar(10) UNIQUE NOT NULL,  -- e.g., en, es, fr
@@ -24,3 +11,17 @@ INSERT INTO public.languages (code, name) VALUES
   ('en', 'English'),
   ('es', 'Spanish')
 ON CONFLICT (code) DO NOTHING;
+
+CREATE TABLE public.profiles (
+  "id" uuid PRIMARY KEY REFERENCES auth.users(id) ON DELETE CASCADE,
+  "email" varchar(255) UNIQUE NOT NULL,
+  "first_name" varchar(50) NOT NULL,
+  "last_name" varchar(50) NOT NULL,
+  "timezone" varchar(50) NOT NULL,
+  "created_at" timestamptz DEFAULT (now()),
+  "updated_at" timestamptz DEFAULT (now()),
+  "native_language_id" uuid NOT NULL REFERENCES public.languages("id"),
+  "target_language_id" uuid NOT NULL REFERENCES public.languages("id"),
+  "target_level" varchar(20) NOT NULL CHECK (target_level IN ('beginner', 'intermediate', 'advanced'))
+);
+
