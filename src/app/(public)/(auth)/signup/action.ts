867e3a92bd6a28efd02be2c6629be5882a2040d3
Seraphim <seraphim.codes@gmail.com>
2025-12-14 09:15:56 -0500

    Cleaned up signup/action.ts code since no longer using redirectTo and just relying on deployed url for confiramtion emails

--- src/app/(public)/(auth)/signup/action.ts
+++ src/app/(public)/(auth)/signup/action.ts
@@ -2,17 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-
 import { createClient } from "@/lib/supabase/server";
-
-const getURL = () => {
-  let url = process?.env?.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000/"; // Automatically set by Vercel.
-  // Make sure to include `https://` when not localhost.
-  url = url.startsWith("http") ? url : `https://${url}`;
-  // Make sure to include a trailing `/`.
-  url = url.endsWith("/") ? url : `${url}/`;
-  return url;
-};
 
 export async function signup(formData: FormData) {
   const supabase = await createClient();
@@ -24,12 +14,7 @@ export async function signup(formData: FormData) {
     password: formData.get("password") as string,
   };
 
-  const { error } = await supabase.auth.signUp({
-    ...data,
-    options: {
-      emailRedirectTo: getURL(),
-    },
-  });
+  const { error } = await supabase.auth.signUp(data);
 
   if (error) {
     console.log("Signup error:", error);
