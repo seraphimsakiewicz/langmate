8fcb5c5b0cb164f08377d3ca128a52a134590fe3
Seraphim <seraphim.codes@gmail.com>
2025-12-14 15:41:39 -0500

    Finished KAN-83, not let users going to signup/profile unless user created and confirmed, redirecting to /signup

--- src/app/(public)/(auth)/signup/profile/page.tsx
+++ src/app/(public)/(auth)/signup/profile/page.tsx
@@ -1,8 +1,17 @@
 import { ProfileForm } from "@/components/auth/profile-form";
 import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
   const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/signup");
+  }
+
   const { data: languages, error } = await supabase
     .from("languages")
     .select("id, name")
@@ -13,7 +22,7 @@ export default async function Page() {
   }
 
   return (
-      <div className="flex min-h-svh w-full overflow-y-auto items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full overflow-y-auto items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-lg">
         <ProfileForm languages={languages || []} />
       </div>
